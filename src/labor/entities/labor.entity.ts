b9747162a582c8eb27f5b17a2265e50e9dadd0ce
gkesh <gkeshthapa@gmail.com>
2023-03-31 15:03:24 +0545

    Defined labor model and added required dependencies

--- src/labor/entities/labor.entity.ts
+++ src/labor/entities/labor.entity.ts
@@ -1,1 +1,20 @@
-export class Labor {}
+enum Proficiency {
+  BEGINNER,
+  INTERMEDIATE,
+  ADVANCED,
+}
+
+class Skill {
+  id: number;
+  title: string;
+  proficiency: Proficiency;
+  experience: number;
+}
+
+export class Labor {
+  id: number;
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  skills: Array<Skill>;
+}
--- src/labor/labor.controller.ts
+++ src/labor/labor.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('labor')
-export class LaborController {}
+export class LaborController {
+  @Get()
+  findAll(@Req() request: Request): string {
+    console.log(request.body);
+    return 'This is a list of all the laborers.';
+  }
+}
